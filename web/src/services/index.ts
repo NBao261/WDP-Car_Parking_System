399c654856b54461e9fae2338ec6746aeba9cc8f
Thái Minh Tuấn <tuantmse181850@fpt.edu.vn>
2026-05-27 16:04:34 +0700

    feat(web): update services index

--- web/src/services/index.ts
+++ web/src/services/index.ts
@@ -1,3 +1,7 @@
 export * from './api';
 export * from './slot.service';
 export * from './pricing.service';
+export * from './session.service';
+export * from './floor.service';
+export * from './vehicleType.service';
+export * from './exception.service';
